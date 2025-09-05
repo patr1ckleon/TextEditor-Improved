23eb6dfa2de60b6f1b7aedd56c960671080d64e3
patr1ckleon <patrick.kimumwe@gmail.com>
2025-09-04 22:48:13 -0400

    adding changes for sql lite and wiring to azure

--- TextEditor/Program.cs
+++ TextEditor/Program.cs
@@ -1,23 +1,46 @@
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.EntityFrameworkCore;
 using TextEditor.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+var useSqlite = builder.Configuration.GetValue<bool>("USE_SQLITE");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
+{
+    if (useSqlite)
+    {
+        var sqlite = builder.Configuration.GetConnectionString("SqliteConnection") ?? "Data Source=app.db";
+        options.UseSqlite(sqlite);
+    }
+    else
+    {
+        var sqlServer = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+        options.UseSqlServer(sqlServer);
+    }
+});
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
     .AddEntityFrameworkStores<ApplicationDbContext>();
-builder.Services.AddControllersWithViews();
+
+// Ensure MVC views and Razor Pages hot-recompile during development
+var mvcBuilder = builder.Services.AddControllersWithViews();
+var pagesBuilder = builder.Services.AddRazorPages();
 #if DEBUG
-builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
+mvcBuilder.AddRazorRuntimeCompilation();
+pagesBuilder.AddRazorRuntimeCompilation();
 #endif
 
 var app = builder.Build();
+
+// Apply migrations automatically at startup (safe for demo/single instance)
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    db.Database.Migrate();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -33,6 +56,12 @@ else
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
+
+// Support reverse proxies (e.g., Vercel/Azure frontends)
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+});
 
 app.UseRouting();
 
